586ece9aa32f917da9fa3c87f9e1536bf63c53b9
Racer911-1 <102369191+Racer911dash1@users.noreply.github.com>
2025-04-29 13:04:23 +0000

    Fixed the Numbering from the results

--- test_ft_putstr_fd.c
+++ test_ft_putstr_fd.c
@@ -65,9 +65,9 @@ int main(void){
 		return (-1);
 	read(fd, res2, strlen(test2));
 	if (strncmp(test2, res2, strlen(test2)) == 0)
-		printf("%s1:OK ", KGRN);
+		printf("%s2:OK ", KGRN);
 	else
-		printf("%s1:KO ", KRED);
+		printf("%s2:KO ", KRED);
 	close(fd);
 	if (remfile() < 0)
 		printf("Error deleting file");
